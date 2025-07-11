09655923f07e76b3ff62f98b0d40e44d4177ea79
levininja <ljh65536@gmail.com>
2025-07-11 11:07:37 -0700

    tone-assigment Initial pass

---
+++ src/levihobbs/Models/ToneAssignmentViewModel.cs
@@ -0,0 +1,33 @@
+namespace levihobbs.Models
+{
+    public class ToneAssignmentViewModel
+    {
+        public List<BookReviewToneItem> BookReviews { get; set; } = new List<BookReviewToneItem>();
+        public List<ToneGroup> ToneGroups { get; set; } = new List<ToneGroup>();
+        public List<ToneDisplayItem> OtherTones { get; set; } = new List<ToneDisplayItem>();
+    }
+
+    public class BookReviewToneItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string AuthorName { get; set; } = "";
+        public List<int> AssignedToneIds { get; set; } = new List<int>();
+        public List<int> SuggestedToneIds { get; set; } = new List<int>();
+    }
+
+    public class ToneGroup
+    {
+        public string Name { get; set; } = "";
+        public string DisplayName { get; set; } = "";
+        public string ColorClass { get; set; } = "";
+        public List<ToneDisplayItem> Tones { get; set; } = new List<ToneDisplayItem>();
+    }
+
+    public class ToneDisplayItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "";
+        public string? Description { get; set; }
+    }
+}
